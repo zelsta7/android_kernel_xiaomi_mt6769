b76e05a3eaa1d0b0b75968f683b8f2422d4198cb
Abderrahmane Benbachir <abderrahmane.benbachir@polymtl.ca>
2018-03-22 20:33:28 -0400

    UPSTREAM: init, tracing: instrument security and console initcall trace events
    Trace events have been added around the initcall functions defined in
    init/main.c. But console and security have their own initcalls. This adds
    the trace events associated for those initcall functions.
    
    Link: http://lkml.kernel.org/r/1521765208.19745.2.camel@polymtl.ca
    
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Change-Id: I37c6d1aac517765230eb698fa0ea0fda7410aef8
    Signed-off-by: Abderrahmane Benbachir <abderrahmane.benbachir@polymtl.ca>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -55,6 +55,7 @@
 #include <linux/uaccess.h>
 #include <asm/sections.h>
 
+#include <trace/events/initcall.h>
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
 
@@ -3218,6 +3219,7 @@ EXPORT_SYMBOL(unregister_console);
  */
 void __init console_init(void)
 {
+	int ret;
 	initcall_t *call;
 
 	/* Setup the default TTY line discipline. */
@@ -3228,8 +3230,11 @@ void __init console_init(void)
 	 * inform about problems etc..
 	 */
 	call = __con_initcall_start;
+	trace_initcall_level("console");
 	while (call < __con_initcall_end) {
-		(*call)();
+		trace_initcall_start((*call));
+		ret = (*call)();
+		trace_initcall_finish((*call), ret);
 		call++;
 	}
 }
