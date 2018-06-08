e90eb35e233f7e5f5af0ab520b387b8171f4bebd
Matthew Wilcox <mawilcox@microsoft.com>
2018-06-07 17:10:45 -0700

    BACKPORT: lib/idr.c: remove simple_ida_lock
    Improve the scalability of the IDA by using the per-IDA xa_lock rather
    than the global simple_ida_lock.  IDAs are not typically used in
    performance-sensitive locations, but since we have this lock anyway, we
    can use it.  It is also a step towards converting the IDA from the radix
    tree to the XArray.
    
    [akpm@linux-foundation.org: idr.c needs xarray.h]
    Link: http://lkml.kernel.org/r/20180331125332.GF13332@bombadil.infradead.org
    Change-Id: I25a1b10e2c0aa8e9b35cca27b488070d7c3ad867
    Signed-off-by: Matthew Wilcox <mawilcox@microsoft.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- lib/idr.c
+++ lib/idr.c
@@ -3,9 +3,9 @@
 #include <linux/idr.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
+#include <linux/xarray.h>
 
 DEFINE_PER_CPU(struct ida_bitmap *, ida_bitmap);
-static DEFINE_SPINLOCK(simple_ida_lock);
 
 int idr_alloc_cmn(struct idr *idr, void *ptr, unsigned long *index,
 		  unsigned long start, unsigned long end, gfp_t gfp,
@@ -465,7 +465,7 @@ again:
 	if (!ida_pre_get(ida, gfp_mask))
 		return -ENOMEM;
 
-	spin_lock_irqsave(&simple_ida_lock, flags);
+	xa_lock_irqsave(&ida->ida_rt, flags);
 	ret = ida_get_new_above(ida, start, &id);
 	if (!ret) {
 		if (id > max) {
@@ -475,7 +475,7 @@ again:
 			ret = id;
 		}
 	}
-	spin_unlock_irqrestore(&simple_ida_lock, flags);
+	xa_unlock_irqrestore(&ida->ida_rt, flags);
 
 	if (unlikely(ret == -EAGAIN))
 		goto again;
@@ -501,8 +501,8 @@ void ida_simple_remove(struct ida *ida, unsigned int id)
 	if ((int)id < 0)
 		return;
 
-	spin_lock_irqsave(&simple_ida_lock, flags);
+	xa_lock_irqsave(&ida->ida_rt, flags);
 	ida_remove(ida, id);
-	spin_unlock_irqrestore(&simple_ida_lock, flags);
+	xa_unlock_irqrestore(&ida->ida_rt, flags);
 }
 EXPORT_SYMBOL(ida_simple_remove);
