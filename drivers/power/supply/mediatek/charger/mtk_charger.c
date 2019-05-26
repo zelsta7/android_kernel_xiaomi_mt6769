2709bbeaadaaf4fec8f5e98d34ce3695c931a57d
chailu1 <chailu1@lenovo.com>
2019-05-26 19:38:14 +0800

    charge: Optimize typeC check
    While check typeC adatper
    Add pointer check
    
    Change-Id: I1e3b896c97813881bd28c3461f8c345aed0682ca
    Signed-off-by: chailu1 <chailu1@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1361522
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1847196
    Reviewed-by: Lu Chai <chailu1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>

--- drivers/power/supply/mediatek/charger/mtk_charger.c
+++ drivers/power/supply/mediatek/charger/mtk_charger.c
@@ -892,6 +892,9 @@ bool is_typec_adapter(struct charger_manager *info)
 {
 	int rp;
 
+	if (info == NULL || info->pd_adapter == NULL)
+		return false;
+
 	rp = adapter_dev_get_property(info->pd_adapter, TYPEC_RP_LEVEL);
 	if (info->pd_type == MTK_PD_CONNECT_TYPEC_ONLY_SNK &&
 			rp != 500 &&
