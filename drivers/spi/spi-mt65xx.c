aaf432c10d6a57dfd82fad87458643e13f872631
wengjun1 <wengjun1@motorola.com>
2021-01-06 16:42:16 +0800

    mt6833: make two spi devices compatible on one spi host
    spi/port from mt6771:
    Make two spi devices compatible on one spi host.
    (Patch from ALPS04384598)
    
    Change-Id: I5846200ac9afbd780f4988097d877a659eee9aa0
    Signed-off-by: wengjun1 <wengjun1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1844528
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jianqiu Huang <huangjq9@motorola.com>
    Reviewed-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-by: Zhihong Kang <kangzh@motorola.com>
    Reviewed-by: Weilong Wu <wuwl10@motorola.com>
    Submit-Approved: Jira Key

--- drivers/spi/spi-mt65xx.c
+++ drivers/spi/spi-mt65xx.c
@@ -887,6 +887,9 @@ static int mtk_spi_probe(struct platform_device *pdev)
 				goto err_put_master;
 			}
 		}
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+		master->num_chipselect = mdata->pad_num;
+#endif
 	}
 
 	platform_set_drvdata(pdev, master);
@@ -974,12 +977,14 @@ static int mtk_spi_probe(struct platform_device *pdev)
 			goto err_disable_runtime_pm;
 		}
 
+#ifndef CONFIG_TARGET_PRODUCT_SELENECOMMON
 		if (!master->cs_gpios && master->num_chipselect > 1) {
 			dev_err(&pdev->dev,
 				"cs_gpios not specified and num_chipselect > 1\n");
 			ret = -EINVAL;
 			goto err_disable_runtime_pm;
 		}
+#endif
 
 		if (master->cs_gpios) {
 			for (i = 0; i < master->num_chipselect; i++) {
