6507550540de090de68a39e6d15364bac3ae497a
LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
2021-12-30 12:20:08 +0200

    drivers/misc: mediatek: adsp: Silence debugging
    Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- drivers/misc/mediatek/adsp/common/adsp_fs.c
+++ drivers/misc/mediatek/adsp/common/adsp_fs.c
@@ -190,7 +190,7 @@ static inline ssize_t wakelock_store(struct device *dev,
 	else if (input == 0)
 		ret = adsp_awake_unlock(pdata->id);
 
-	pr_info("%s, input %u, ret = %d", __func__, input, ret);
+	pr_debug("%s, input %u, ret = %d", __func__, input, ret);
 
 	return count;
 }
@@ -374,7 +374,7 @@ static long adsp_driver_ioctl(
 	}
 
 	if (ret < 0)
-		pr_info("%s(), ioctl error %d\n", __func__, ret);
+		pr_debug("%s(), ioctl error %d\n", __func__, ret);
 
 	return ret;
 }
