309295ee7e72020e8cc02d80cec79f4fd5ef56ce
Zhang Qilong <zhangqilong3@huawei.com>
2022-08-31 17:48:15 +0800

    BACKPORT: f2fs: add static init_idisk_time function to reduce the code
    We can use a inner function to init the disk time
    of f2fs_inode_info for cleaning redundant code.
    
    Change-Id: Iafd6cd55a764372c17b7eb90931336a9b9727f1a
    Signed-off-by: Zhang Qilong <zhangqilong3@huawei.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -320,6 +320,16 @@ static bool sanity_check_inode(struct inode *inode, struct page *node_page)
 	return true;
 }
 
+static void init_idisk_time(struct inode *inode)
+{
+	struct f2fs_inode_info *fi = F2FS_I(inode);
+
+	fi->i_disk_time[0] = inode->i_atime;
+	fi->i_disk_time[1] = inode->i_ctime;
+	fi->i_disk_time[2] = inode->i_mtime;
+	fi->i_disk_time[3] = fi->i_crtime;
+}
+
 static int do_read_inode(struct inode *inode)
 {
 	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
@@ -450,10 +460,7 @@ static int do_read_inode(struct inode *inode)
 		}
 	}
 
-	F2FS_I(inode)->i_disk_time[0] = timespec64_to_timespec(inode->i_atime);
-	F2FS_I(inode)->i_disk_time[1] = timespec64_to_timespec(inode->i_ctime);
-	F2FS_I(inode)->i_disk_time[2] = timespec64_to_timespec(inode->i_mtime);
-	F2FS_I(inode)->i_disk_time[3] = F2FS_I(inode)->i_crtime;
+	init_idisk_time(inode);
 	f2fs_put_page(node_page, 1);
 
 	stat_inc_inline_xattr(inode);
@@ -633,11 +640,15 @@ void f2fs_update_inode(struct inode *inode, struct page *node_page)
 	if (inode->i_nlink == 0)
 		clear_inline_node(node_page);
 
+<<<<<<< HEAD
 	F2FS_I(inode)->i_disk_time[0] = timespec64_to_timespec(inode->i_atime);
 	F2FS_I(inode)->i_disk_time[1] = timespec64_to_timespec(inode->i_ctime);
 	F2FS_I(inode)->i_disk_time[2] = timespec64_to_timespec(inode->i_mtime);
 	F2FS_I(inode)->i_disk_time[3] = F2FS_I(inode)->i_crtime;
 
+=======
+	init_idisk_time(inode);
+>>>>>>> 1a5791299b02 (BACKPORT: f2fs: add static init_idisk_time function to reduce the code)
 #ifdef CONFIG_F2FS_CHECK_FS
 	f2fs_inode_chksum_set(F2FS_I_SB(inode), node_page);
 #endif
