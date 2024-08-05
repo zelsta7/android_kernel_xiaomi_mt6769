41c3a45a10a5e4ea39b9ce267df26ecc949ad43f
Viole403 <m.maulana.fb26@gmail.com>
2024-08-06 00:52:14 +0700

    fix(mtk_wlan_gen4m_fw_dl): Suppress ROM code capability message
    - Removed the commented-out pragma messages in `fw_dl.c` to eliminate compiler warnings.
    - These messages indicated ROM code support for SEM-CTRL during ROM patch download.
    
    This commit improves code cleanliness and reduces unnecessary compiler warnings.

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/chips/common/fw_dl.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/chips/common/fw_dl.c
@@ -2399,7 +2399,7 @@ uint32_t wlanDownloadPatch(IN struct ADAPTER *prAdapter)
 	}
 
 #if (CFG_ROM_PATCH_NO_SEM_CTRL == 0)
-#pragma message("ROM code supports SEM-CTRL for ROM patch download")
+//#pragma message("ROM code supports SEM-CTRL for ROM patch download")
 	if (wlanPatchIsDownloaded(prAdapter)) {
 		kalFirmwareImageUnmapping(prAdapter->prGlueInfo, NULL,
 					  prFwBuffer);
@@ -2407,7 +2407,7 @@ uint32_t wlanDownloadPatch(IN struct ADAPTER *prAdapter)
 		return WLAN_STATUS_SUCCESS;
 	}
 #else
-#pragma message("ROM code supports no SEM-CTRL for ROM patch download")
+//#pragma message("ROM code supports no SEM-CTRL for ROM patch download")
 #endif
 
 	/* Patch DL */
