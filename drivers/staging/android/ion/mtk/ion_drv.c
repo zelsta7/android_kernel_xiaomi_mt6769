89a64e971baf23716fc6ff7474c28438a8681173
Viole403 <m.maulana.fb26@gmail.com>
2024-08-05 23:17:29 +0700

    fix(ion_drv): Address casting and unused variable warnings
    - Ensured type safety by replacing `(unsigned int)` cast with `(unsigned long)` in `m4u_mva_unmap_kernel`.
    - Improved code clarity by comment the unused variable `heap`.

--- drivers/staging/android/ion/mtk/ion_drv.c
+++ drivers/staging/android/ion/mtk/ion_drv.c
@@ -441,7 +441,7 @@ static long ion_sys_cache_sync(struct ion_client *client,
 	unsigned long kernel_va = 0;
 	unsigned long kernel_size = 0;
 	struct sg_table *table;
-	struct ion_heap *heap = NULL;
+	//struct ion_heap *heap = NULL;
 	int is_kernel_addr = from_kernel;
 
 	/* Get kernel handle
@@ -563,7 +563,7 @@ static long ion_sys_cache_sync(struct ion_client *client,
 		m4u_mva_unmap_kernel((unsigned long)param->va,
 				     sync_size, sync_va);
 #else
-		m4u_mva_unmap_kernel((unsigned int)param->va,
+		m4u_mva_unmap_kernel((unsigned long)param->va,
 				     (unsigned int)sync_size, sync_va);
 #endif
 	} else if (ion_need_unmap_flag) {
