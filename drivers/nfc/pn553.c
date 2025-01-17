97badb4ae22d9bad5f7a3c84ed2c4962ff82d44f
fukiame <fukiame@proton.me>
2025-01-17 08:32:52 +0700

    drivers: nfc: Import Xiaomi changes from selene-r-oss-update

--- drivers/nfc/pn553.c
+++ drivers/nfc/pn553.c
@@ -88,7 +88,9 @@ extern int force_sig_info(int, struct siginfo *, struct task_struct *);
 
 /* VEN is kept ON all the time if you define the macro VEN_ALWAYS_ON.
 Used for SN100 usecases */
+#ifndef CONFIG_TARGET_PRODUCT_SELENECOMMON
 #define VEN_ALWAYS_ON
+#endif
 /* Macro added to disable SVDD power toggling */
 /* #define JCOP_4X_VALIDATION */
 
