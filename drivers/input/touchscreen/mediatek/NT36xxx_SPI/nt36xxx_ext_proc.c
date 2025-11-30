6e6d9136d681fd274a5a7d4f9e69fd16f0b5b6cf
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2025-11-30 12:03:03 +0000

    touchscreen: NT36xxx_SPI: Optimize c_tp_info_show

--- drivers/input/touchscreen/mediatek/NT36xxx_SPI/nt36xxx_ext_proc.c
+++ drivers/input/touchscreen/mediatek/NT36xxx_SPI/nt36xxx_ext_proc.c
@@ -1848,17 +1848,23 @@ return:
 /* Huaqin modify for HQ-123470 by shujiawang at 2021/03/29 start */
 static int32_t c_tp_info_show(struct seq_file *m, void *v)
 {
+#ifdef CONFIG_TARGET_PRODUCT_MERLINCOMMON
+	seq_printf(m, "[Vendor]Tianma,[TP-IC]:NT36672A,[FW]0x%x,PID=%04X\n", ts->fw_ver, ts->nvt_pid);
+#else
 	if (is_ft_lcm == 0) {
 		seq_printf(m, "[Vendor]Tianma,[TP-IC]:NT36672,[FW]0x%x,PID=%04X\n", ts->fw_ver, ts->nvt_pid);
 	} else if (is_ft_lcm == 1) {
 		seq_printf(m, "[Vendor]Dijing,[TP-IC]:NT36672,[FW]0x%x,PID=%04X\n", ts->fw_ver, ts->nvt_pid);
 	} else if (is_ft_lcm == 3) {
 		seq_printf(m, "[Vendor]Dijing,[TP-IC]:NT36672D,[FW]0x%x,PID=%04X\n", ts->fw_ver, ts->nvt_pid);
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
 	} else if (is_ft_lcm == 4) {
 		seq_printf(m, "[Vendor]Tianma, [TP-IC]:nt36672C,[FW]0x%x,PID=%04X\n", ts->fw_ver, ts->nvt_pid);
 	} else if (is_ft_lcm == 5) {
 		seq_printf(m, "[Vendor]Truly, [TP-IC]:nt36672C,[FW]0x%x,PID=%04X\n", ts->fw_ver, ts->nvt_pid);
+#endif
 	}
+#endif
 
 	return 0;
 }
