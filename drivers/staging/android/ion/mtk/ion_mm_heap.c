dbb2184a18bca31af5c003d33f47cb9964e6b6e6
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-01-11 17:57:49 +0000

    treewide: More fix for totalram_pages usage (MTK)

--- drivers/staging/android/ion/mtk/ion_mm_heap.c
+++ drivers/staging/android/ion/mtk/ion_mm_heap.c
@@ -362,7 +362,7 @@ static int ion_mm_heap_allocate(struct ion_heap *heap,
 	unsigned long user_va = 0;
 
 	INIT_LIST_HEAD(&pages);
-	if (size / PAGE_SIZE > totalram_pages / 2) {
+	if (size / PAGE_SIZE > totalram_pages() / 2) {
 		IONMSG("%s size %lu is larger than totalram_pages.\n", __func__,
 		       size);
 		return -ENOMEM;
