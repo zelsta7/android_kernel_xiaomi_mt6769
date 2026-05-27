0f35974670ed5383a21d9ccdd23e31d5c185182d
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-05-27 15:32:11 +0000

    sound/: Import Xiaomi kernel changes from selene-r-oss-update

--- sound/soc/codecs/mt6358.c
+++ sound/soc/codecs/mt6358.c
@@ -1859,7 +1859,9 @@ static int mtk_hp_spk_enable(struct mt6358_priv *priv)
 				0xff, 0x0004);
 
 	/* Audio left headphone input multiplexor selection : LOL */
+#ifndef CONFIG_TARGET_PRODUCT_SELENECOMMON
 	set_hp_l_input_mux(priv, HP_INPUT_MUX_LOL);
+#endif
 
 	/* Disable headphone short-circuit protection */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON0,
@@ -1926,12 +1928,20 @@ static int mtk_hp_spk_enable(struct mt6358_priv *priv)
 	/* Set LOL gain to normal gain step by step */
 	regmap_write(priv->regmap, MT6358_ZCD_CON1, DL_GAIN_N_10DB_REG);
 
+#ifndef CONFIG_TARGET_PRODUCT_SELENECOMMON
 	/* Switch HPL MUX to Line-out */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON0,
 			0x3 << 8, 0x01 << 8);
 	/* Switch HPR MUX to DAC-R */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON0,
 			0x3 << 10, 0x2 << 10);
+#else
+	/* Switch HPL MUX to HS */
+	regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON0, 0x3300);
+	/* Switch HPR MUX to LOL */
+	regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON0, 0x3700);
+#endif
+
 	/* Enable HP aux output stage */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON1,
 			0xff, 0x0c);
@@ -2004,7 +2014,20 @@ static int mtk_hp_spk_enable(struct mt6358_priv *priv)
 
 	/* Switch LOL MUX to audio DAC */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON7,
+#ifndef CONFIG_TARGET_PRODUCT_SELENECOMMON
 			0x3 << 2, 0x2 << 2);
+#else
+			0x3 << 2, 0x1 << 2);
+#endif
+
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	/* Switch HS MUX to audio DAC */
+	regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON6, 0x009b);
+
+	//lch inverse
+	regmap_update_bits(priv->regmap, MT6358_AFUNC_AUD_CON0,
+		0x4000, 0x1<<10);
+#endif
 
 	/* Disable Pull-down HPL/R to AVSS28_AUD */
 	hp_pull_down(priv, false);
@@ -2428,6 +2451,12 @@ static int mtk_hp_dual_spk_disable(struct mt6358_priv *priv)
 	/* Set HP CMFB gate rstb */
 	regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON4,
 			0x1 << 6, 0x0);
+
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	/* Switch HS MUX to audio DAC */
+	regmap_write(priv->regmap, MT6358_AUDDEC_ANA_CON6, 0x0090);
+#endif
+
 	/* disable Pull-down HPL/R to AVSS28_AUD */
 	hp_pull_down(priv, false);
 
@@ -2635,6 +2664,9 @@ static int mt_lo_event(struct snd_soc_dapm_widget *w,
 {
 	struct snd_soc_component *cmpnt = snd_soc_dapm_to_component(w->dapm);
 	struct mt6358_priv *priv = snd_soc_component_get_drvdata(cmpnt);
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	uint32_t reg_value = 0;
+#endif
 
 	dev_info(priv->dev, "%s(), event 0x%x, mux %u\n",
 		 __func__,
@@ -2718,6 +2750,16 @@ static int mt_lo_event(struct snd_soc_dapm_widget *w,
 		regmap_update_bits(priv->regmap, MT6358_AUDDEC_ANA_CON7,
 				   0x3 << 2, 0x1 << 2);
 
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	//lch inverse
+	 regmap_write(priv->regmap, MT6358_AFUNC_AUD_CON0,0xCFA1);
+#endif
+
+#ifdef CONFIG_TARGET_PRODUCT_SELENECOMMON
+	//add by mtk for debug 
+        regmap_read(priv->regmap, MT6358_AFUNC_AUD_CON0, &reg_value);
+        dev_info(priv->dev, "%s(), MT6358_AFUNC_AUD_CON0 0x%x, \n",__func__,reg_value);
+#endif
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
 		/* Switch LOL MUX to open */
--- sound/soc/mediatek/mt6768/mt6768-misc-control.c
+++ sound/soc/mediatek/mt6768/mt6768-misc-control.c
@@ -23,6 +23,10 @@
 #define SGEN_MUTE_CH1_KCONTROL_NAME "Audio_SineGen_Mute_Ch1"
 #define SGEN_MUTE_CH2_KCONTROL_NAME "Audio_SineGen_Mute_Ch2"
 
+#ifdef CONFIG_SND_SOC_FS16XX
+extern int fsm_add_control(struct snd_soc_platform *platform);
+#endif
+
 static const char * const mt6768_sgen_mode_str[] = {
 	"I0I1",   "I2",     "I3I4",   "I5I6",
 	"I7I8",   "I9",     "I10I11", "I12I13",
@@ -1249,6 +1253,11 @@ int mt6768_add_misc_control(struct snd_soc_platform *platform)
 	snd_soc_add_platform_controls(platform,
 				      mt6768_afe_speech_controls,
 				      ARRAY_SIZE(mt6768_afe_speech_controls));
+
+#ifdef CONFIG_SND_SOC_FS16XX
+	fsm_add_control(platform);
+#endif
+
 #if defined(CONFIG_MTK_VOW_BARGE_IN_SUPPORT)
 	snd_soc_add_platform_controls(platform,
 				      mt6768_afe_bargein_controls,
