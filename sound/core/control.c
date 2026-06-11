14a639e5d1aef33d05f84bf8a68d8d32ad106d4f
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-06-11 23:03:27 +0000

    Revert "[ALPS07919445] ASoC: UPSTREAM: ALSA: pcm: Move rwsem lock inside snd_ctl_elem_read to prevent UAF"
    This reverts commit 3027e84a84e3e7da73fa29bc9769ab49b7e84691.

--- sound/core/control.c
+++ sound/core/control.c
@@ -888,29 +888,18 @@ static int snd_ctl_elem_read(struct snd_card *card,
 	struct snd_kcontrol *kctl;
 	struct snd_kcontrol_volatile *vd;
 	unsigned int index_offset;
-	int ret;
 
-	down_read(&card->controls_rwsem);
 	kctl = snd_ctl_find_id(card, &control->id);
-	if (kctl == NULL) {
-		ret = -ENOENT;
-		goto unlock;
-	}
+	if (kctl == NULL)
+		return -ENOENT;
 
 	index_offset = snd_ctl_get_ioff(kctl, &control->id);
 	vd = &kctl->vd[index_offset];
-	if (!(vd->access & SNDRV_CTL_ELEM_ACCESS_READ) || kctl->get == NULL) {
-		ret = -EPERM;
-		goto unlock;
-	}
+	if (!(vd->access & SNDRV_CTL_ELEM_ACCESS_READ) || kctl->get == NULL)
+		return -EPERM;
 
 	snd_ctl_build_ioff(&control->id, kctl, index_offset);
-	ret = kctl->get(kctl, control);
-	if (ret < 0)
-		goto unlock;
-unlock:
-	up_read(&card->controls_rwsem);
-	return ret;
+	return kctl->get(kctl, control);
 }
 
 static int snd_ctl_elem_read_user(struct snd_card *card,
@@ -927,7 +916,9 @@ static int snd_ctl_elem_read_user(struct snd_card *card,
 	if (result < 0)
 		goto error;
 
+	down_read(&card->controls_rwsem);
 	result = snd_ctl_elem_read(card, control);
+	up_read(&card->controls_rwsem);
 	if (result < 0)
 		goto error;
 
