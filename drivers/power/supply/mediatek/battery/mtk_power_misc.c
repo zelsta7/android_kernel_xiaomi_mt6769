77216c9347b6a42cd7dcae36e3a567a2a57d6a08
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-06-21 10:07:14 +0000

    Revert "drivers/power/supply: Import OEM changes"
    This reverts commit a1082734bd2cf59e452fb830eb6d8e214312af1c.

--- drivers/power/supply/mediatek/battery/mtk_power_misc.c
+++ drivers/power/supply/mediatek/battery/mtk_power_misc.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2016 MediaTek Inc.
- * Copyright (C) 2021 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -71,13 +70,13 @@ static void wake_up_power_misc(struct shutdown_controller *sdd)
 	sdd->timeout = true;
 	wake_up(&sdd->wait_que);
 }
-#if 0  /*zhouhua remove 60 temp kernel shutdown*/
+
 static void wake_up_overheat(struct shutdown_controller *sdd)
 {
 	sdd->overheat = true;
 	wake_up(&sdd->wait_que);
 }
-#endif
+
 void set_shutdown_vbat_lt(int vbat_lt, int vbat_lt_lv1)
 {
 	g_vbat_lt = vbat_lt;
@@ -545,7 +544,7 @@ int mtk_power_misc_psy_event(
 					"battery temperature >= %d,shutdown",
 					tmp);
 
-				//wake_up_overheat(&sdc);   /*zhouhua remove 60 temp kernel shutdown*/
+				wake_up_overheat(&sdc);
 			}
 		}
 	}
