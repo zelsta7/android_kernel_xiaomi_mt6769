a0635cc629405c591a471884d1e4242f83f7b7a4
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-06-21 10:10:58 +0000

    Revert "drivers/: touchscreen: Import Xiaomi changes from `rosemary-r-oss`"
    This reverts commit 8750be191a2e635512f322f9aa5e9eacd3e3ae05.

--- drivers/input/touchscreen/mediatek/mtk_tpd.c
+++ drivers/input/touchscreen/mediatek/mtk_tpd.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2016 MediaTek Inc.
- * Copyright (C) 2021 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -50,9 +49,6 @@ struct pinctrl *pinctrl1;
 struct pinctrl_state *pins_default;
 struct pinctrl_state *eint_as_int, *eint_output0,
 		*eint_output1, *rst_output0, *rst_output1;
-#if defined(CONFIG_TOUCHSCREEN_COMMON)
-bool tpd_gesture_flag;
-#endif
 const struct of_device_id touch_of_match[] = {
 	{ .compatible = "mediatek,touch", },
 	{ .compatible = "mediatek,mt8167-touch", },
