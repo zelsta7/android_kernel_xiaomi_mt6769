40006d2408a99d27596e507a63bfa3477efffb83
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2026-06-21 09:35:32 +0000

    Revert "drivers: connectivity: gen4m: Silence more debug logging"
    This reverts commit 5b8dbda1b92a30e18e79609f32106b75bab565a2.

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/gl_kal.c
@@ -9774,7 +9774,6 @@ void kalUpdateCompHdlrRec(IN struct ADAPTER *prAdapter,
 					% OID_HDLR_REC_NUM;
 }
 
-#if (BUILD_QA_DBG == 1)
 void kalPrintUTC(char *msg_buf, int msg_buf_size)
 {
 	int ret = 0;
@@ -9857,7 +9856,7 @@ void kalPrintLog(const char *fmt, ...)
 
 	va_end(args);
 }
-#endif
+
 
 #if (CFG_SUPPORT_POWER_THROTTLING == 1)
 void kalPwrLevelHdlrRegister(IN struct ADAPTER *prAdapter,
--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/include/gl_kal.h
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/include/gl_kal.h
@@ -1053,7 +1053,6 @@ int8_t atoi(uint8_t ch);
 
 #define kalGetTimeTick()                jiffies_to_msecs(jiffies)
 
-#if (BUILD_QA_DBG == 1)
 #define kalPrintLogLimited(fmt, ...)					\
 ({									\
 	static DEFINE_RATELIMIT_STATE(_rs,				\
@@ -1066,10 +1065,6 @@ int8_t atoi(uint8_t ch);
 #define WLAN_TAG                        "[wlan]"
 #define kalPrint               kalPrintLog
 #define kalPrintLimited(_Fmt...) kalPrintLogLimited(WLAN_TAG _Fmt)
-#else
-#define kalPrintLimited(fmt, ...)
-#define kalPrint(fmt, ...)
-#endif
 
 #define kalBreakPoint() \
 do { \
@@ -1957,9 +1952,7 @@ void kalUpdateCompHdlrRec(IN struct ADAPTER *prAdapter,
 	IN PFN_OID_HANDLER_FUNC pfnOidHandler, IN struct CMD_INFO *prCmdInfo);
 
 extern uint32_t get_wifi_standalone_log_mode(void);
-#if (BUILD_QA_DBG == 1)
 void kalPrintLog(const char *fmt, ...);
-#endif
 
 #if (CFG_SUPPORT_POWER_THROTTLING == 1)
 void kalPwrLevelHdlrRegister(IN struct ADAPTER *prAdapter,
