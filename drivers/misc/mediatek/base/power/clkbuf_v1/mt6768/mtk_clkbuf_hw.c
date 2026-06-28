b9ce2c7dcb109f5d5430fa384efc44163ca95fae
vrdons <vrdons@proton.me>
2026-06-28 18:38:07 +0300

    2

--- drivers/misc/mediatek/base/power/clkbuf_v1/mt6768/mtk_clkbuf_hw.c
+++ drivers/misc/mediatek/base/power/clkbuf_v1/mt6768/mtk_clkbuf_hw.c
@@ -790,25 +790,25 @@ void clk_buf_disp_ctrl(bool onoff)
 	pwrap_dcxo_en = clkbuf_readl(DCXO_ENABLE) & ~DCXO_NFC_ENABLE;
 	clkbuf_writel(DCXO_ENABLE, pwrap_dcxo_en);
 	if (onoff) {
-		pmic_config_interface(PMIC_DCXO_CW00_CLR_ADDR,
-			PMIC_XO_EXTBUF3_MODE_MASK,
-			PMIC_XO_EXTBUF3_MODE_MASK,
-			PMIC_XO_EXTBUF3_MODE_SHIFT);
-		pmic_config_interface(PMIC_DCXO_CW00_SET_ADDR,
-			PMIC_XO_EXTBUF3_EN_M_MASK,
-			PMIC_XO_EXTBUF3_EN_M_MASK,
-			PMIC_XO_EXTBUF3_EN_M_SHIFT);
-		pmic_clk_buf_swctrl[XO_NFC] = 1;
+		pmic_config_interface(PMIC_DCXO_CW11_CLR_ADDR,
+					 PMIC_XO_EXTBUF7_MODE_MASK,
+					 PMIC_XO_EXTBUF7_MODE_MASK,
+					 PMIC_XO_EXTBUF7_MODE_SHIFT);
+		pmic_config_interface(PMIC_DCXO_CW11_SET_ADDR,
+					 PMIC_XO_EXTBUF7_EN_M_MASK,
+					 PMIC_XO_EXTBUF7_EN_M_MASK,
+					 PMIC_XO_EXTBUF7_EN_M_SHIFT);
+		pmic_clk_buf_swctrl[XO_EXT] = 1;
 	} else {
-		pmic_config_interface(PMIC_DCXO_CW00_CLR_ADDR,
-			PMIC_XO_EXTBUF3_MODE_MASK,
-			PMIC_XO_EXTBUF3_MODE_MASK,
-			PMIC_XO_EXTBUF3_MODE_SHIFT);
-		pmic_config_interface(PMIC_DCXO_CW00_CLR_ADDR,
-			PMIC_XO_EXTBUF3_EN_M_MASK,
-			PMIC_XO_EXTBUF3_EN_M_MASK,
-			PMIC_XO_EXTBUF3_EN_M_SHIFT);
-		pmic_clk_buf_swctrl[XO_NFC] = 0;
+		pmic_config_interface(PMIC_DCXO_CW11_CLR_ADDR,
+					 PMIC_XO_EXTBUF7_MODE_MASK,
+					 PMIC_XO_EXTBUF7_MODE_MASK,
+					 PMIC_XO_EXTBUF7_MODE_SHIFT);
+		pmic_config_interface(PMIC_DCXO_CW11_CLR_ADDR,
+					 PMIC_XO_EXTBUF7_EN_M_MASK,
+					 PMIC_XO_EXTBUF7_EN_M_MASK,
+					 PMIC_XO_EXTBUF7_EN_M_SHIFT);
+		pmic_clk_buf_swctrl[XO_EXT] = 0;
 	}
 }
 EXPORT_SYMBOL(clk_buf_disp_ctrl);
