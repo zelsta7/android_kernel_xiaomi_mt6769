20c94e4d3a3355ac42eb7613ec3cd170975a140b
vrdons <vrdons@proton.me>
2026-06-28 14:48:05 +0300

    Not the problem

--- fs/crypto/inline_crypt.c
+++ fs/crypto/inline_crypt.c
@@ -368,12 +368,9 @@ static void fscrypt_check_hie_ext4(struct bio *bio, const struct inode *inode)
 		if (ci->ci_inode->i_sb->s_magic == EXT4_SUPER_MAGIC) {
 			bc->hie_ext4 = true;
 			bc->hie_f2fs_legacy = false;
-		} else if (ci->ci_inode->i_sb->s_magic == F2FS_SUPER_MAGIC) {
-			bc->hie_ext4 = false;
-			bc->hie_f2fs_legacy = true;
 		} else {
 			bc->hie_ext4 = false;
-			bc->hie_f2fs_legacy = false;  // unknown fs, no legacy
+			bc->hie_f2fs_legacy = true;
 		}
 	} else {
 		bc->hie_ext4 = false;
