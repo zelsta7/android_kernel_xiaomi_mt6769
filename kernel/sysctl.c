8bdcf4e9a70bc499c1db46f54cfc35b2a53b8818
MD. Jadly Bin Salam Shovon <shovon129131@gmail.com>
2026-07-09 16:17:47 +0600

    Add unused variable 'three' to sysctl.c

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -137,6 +137,7 @@ static int __maybe_unused neg_one = -1;
 static int zero;
 static int __maybe_unused one = 1;
 static int __maybe_unused two = 2;
+static int __maybe_unused three = 3;
 static int __maybe_unused four = 4;
 static int int_max = INT_MAX;
 static unsigned long zero_ul;
