4def054bccc7c5bd56d891cac8144ad4feaaa571
vrdons <vrdons@proton.me>
2026-08-18 14:53:58 +0300

    Revert "[ALPS04783265] FROMLIST: mm: process reclaim: skip locked vmas"
    This reverts commit b357674183f5a069206686818a43c2a525d69188.

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1822,9 +1822,6 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 		for (vma = mm->mmap; vma; vma = vma->vm_next) {
 			reclaim_walk.private = vma;
 
-			if (vma->vm_flags & VM_LOCKED)
-				continue;
-
 			if (is_vm_hugetlb_page(vma))
 				continue;
 
