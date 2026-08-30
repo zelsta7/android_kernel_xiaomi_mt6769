a062f167a7586acbd54a7589156c82eb690cbf8c
Sabri Eren <kittydunyasifan@gmail.com>
2026-08-30 04:06:32 +0300

    selene: KernelSU-Next + SuSFS v2.2.0 + SquashFS + BBR
    Cihazda dogrulandi: 4.14.357+17-LittleSpammyMailman-legacy-v0.1-pre
    boot ediyor, root calisiyor, susfs v2.2.0 aktif.
    
    KernelSU-Next v3.1.0-legacy-susfs secildi cunku:
      - cihazdaki yonetici com.rifsxd.ksunext (ksud 3.2.0), yani KSU-Next sart
      - 4.14 icin MANUEL hook gerekiyor; KSU Kconfig i kprobes/tracepoint
        hook u icin "should not be used on kernel below 5.10" diyor
      - bu etiket duz yerlesimde ve Kconfig inde CONFIG_KSU_SUSFS hazir geliyor
    
    Manuel hook lar fs/exec.c, fs/open.c, fs/read_write.c, fs/stat.c,
    drivers/input/input.c ve kernel/reboot.c ye eklendi. Yerlesim bu cihazda
    calisan dieD cekirdeginden alindi, ancak imzalar bu KSU surumune gore
    duzeltildi: ksu_handle_sys_read dieD de uc parametreliyken burada tek
    parametreli ve void.
    
    reboot.c hook u ns_capable(CAP_SYS_BOOT) kontrolunden ONCE duruyor: bu
    KSU surumu super cagri kanali olarak sys_reboot kullaniyor ve yonetici
    uygulamasi root degil, sonra konsaydi cagri -EPERM ile donerdi.
    
    susfs: agac zaten v2.2.0 NON-GKI ile geliyordu, KSU ise v2.0.0 API sine
    gore yazilmis. Uc noktada sapma vardi, patches/ksu altinda kayitli:
    susfs_set_i_state_on_external_dir, susfs_reorder_mnt_id ve
    susfs_run_sus_path_loop artik v2.2.0 da DAHILI (sdcard monitor kthread).
    
    Ayrica SquashFS (tum sikistiricilar) ve TCP BBR + fq_codel.

--- fs/exec.c
+++ fs/exec.c
@@ -1899,12 +1899,28 @@ int do_execve_file(struct file *file, void *__argv, void *__envp)
 	return __do_execve_file(AT_FDCWD, NULL, argv, envp, 0, file);
 }
 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+		void *argv, void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+		void *argv, void *envp, int *flags);
+#endif
+
 int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv,
+				&envp, 0);
+	else
+		ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename,
+					NULL, NULL, NULL);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
@@ -1932,6 +1948,12 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
+	/* 32-bit su */
+	if (!ksu_execveat_hook)
+		ksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename,
+					NULL, NULL, NULL);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
