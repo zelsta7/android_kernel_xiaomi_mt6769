29547a7e9ef9ac71e2303f54b661ed34fa971367
zelsta7 <adrishg130@gmail.com>
2026-09-02 22:46:29 +0530

    Refactor newfstatat to simplify ksu handling
    Removed conditional handling for ksu in newfstatat syscall.

--- fs/stat.c
+++ fs/stat.c
@@ -382,10 +382,6 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 }
 
 #if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)
-#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user,
-		int *flags);
-#endif
 
 SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 		struct stat __user *, statbuf, int, flag)
@@ -393,11 +389,7 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
-#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
-	ksu_handle_stat(&dfd, &filename, &flag);
-#endif
-
-	error = vfs_fstatat(dfd, filename, &stat, flag);
+   error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
 		return error;
 	return cp_new_stat(&stat, statbuf);
@@ -541,12 +533,7 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
-#if defined(CONFIG_KSU) && !defined(CONFIG_KSU_KPROBES_HOOK)
-	/* 32-bit su */
-	ksu_handle_stat(&dfd, &filename, &flag);
-#endif
-
-	error = vfs_fstatat(dfd, filename, &stat, flag);
+    error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
 		return error;
 	return cp_new_stat64(&stat, statbuf);
